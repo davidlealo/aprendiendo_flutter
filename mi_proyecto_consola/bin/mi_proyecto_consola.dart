b14dbe1c61d147859475e6636ed2296fd50af0c3
davidlealo <davidlealo@gmail.com>
2024-10-26 21:43:24 -0300

    proyecto com dart

---
+++ mi_proyecto_consola/bin/mi_proyecto_consola.dart
@@ -0,0 +1,5 @@
+import 'package:mi_proyecto_consola/mi_proyecto_consola.dart' as mi_proyecto_consola;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mi_proyecto_consola.calculate()}!');
+}
---
+++ mi_proyecto_consola/test/mi_proyecto_consola_test.dart
@@ -0,0 +1,8 @@
+import 'package:mi_proyecto_consola/mi_proyecto_consola.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
