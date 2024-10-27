4c9890bcccf157f6d149d03fce2fb79bb6299112
davidlealo <davidlealo@gmail.com>
2024-10-26 22:09:59 -0300

    print de variables

--- mi_proyecto_consola/bin/variables.dart
+++ mi_proyecto_consola/bin/variables.dart
@@ -2,12 +2,17 @@ main (){
   // Datos numéricos
   int variableEnteros = 2;
   double variablesDecimales = 4.5;
+  print("variableEnteros $variableEnteros");
+  print("variablesDecimales $variablesDecimales");
+
 
   // Cadenas de texto
   String variableTexto = 'Este es un texto';
+  print("variableTexto $variableTexto");
 
   // Variables booleanas
   bool variableBooleana = true;
+  print("variableBooleana $variableBooleana");
 
   // datis dinámicos
   var dinamico = 10.9;
