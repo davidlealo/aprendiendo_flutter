2a914cdb1a1143e7b1b87d530f720666349b59ff
davidlealo <davidlealo@gmail.com>
2024-10-26 21:58:39 -0300

    tipos de datos

---
+++ mi_proyecto_consola/bin/variables.dart
@@ -0,0 +1,14 @@
+main (){
+  // Datos numéricos
+  int variableEnteros = 2;
+  double variablesDecimales = 4.5;
+
+  // Cadenas de texto
+  String variableTexto = 'Este es un texto';
+
+  // Variables booleanas
+  bool variableBooleana = true;
+
+  // datis dinámicos
+  var dinamico = 10.9;
+}
