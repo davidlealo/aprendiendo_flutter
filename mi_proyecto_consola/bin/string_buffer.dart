ef7f4f4d411d2ea45cb6d80b2d7d7617107fba55
davidlealo <davidlealo@gmail.com>
2024-10-30 22:11:22 -0300

    midiendo el cambio en las cadenas

--- mi_proyecto_consola/bin/string_buffer.dart
+++ mi_proyecto_consola/bin/string_buffer.dart
@@ -2,4 +2,9 @@ main(List<String> args){
   StringBuffer buffer = StringBuffer();
 
   buffer.write('Este es un texto');
+  print(buffer.length);
+
+  buffer.writeAll(['primera', 'segunda', 'tercera']);
+  print(buffer.length);
+
 }
