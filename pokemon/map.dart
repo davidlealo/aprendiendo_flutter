0a20a8ee86aaf7fdf999ce8a8418f854de964f7c
davidlealo <davidlealo@gmail.com>
2024-11-15 23:31:21 -0300

    map inside a map

--- pokemon/map.dart
+++ pokemon/map.dart
@@ -5,6 +5,10 @@ void main (){
     'hp': 100,
     'isAlive': true,
     'abilities': <String>['impostor'],
+    'sprites': <int, String> {
+      1: 'ditto/front.png',
+      2: 'ditto/back.png'
+    },
   };
 
   print(pokemon);
